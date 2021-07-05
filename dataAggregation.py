ba8b4347073683881a1a20d86458e3e15dad1306
wfischer42 <william.fischer42@gmail.com>
2021-07-05 13:16:00 -0400

    Attempt to run data agg script using remote csv

--- dataAggregation.py
+++ dataAggregation.py
@@ -7,7 +7,8 @@ from datetime import timedelta
 today_date = (date.today() - timedelta(days=1)).strftime("%Y-%m-%d")
 
 yesterday_date = (date.today() - timedelta(days=2)).strftime("%Y-%m-%d")
-df = pd.read_csv("us-counties.csv")
+df = pd.read_csv("http://raw.githubusercontent.com/nytimes/covid-19-data/master/us-counties.csv")
+# df = pd.read_csv("us-counties.csv")
 today_grouped = df[df["date"] == today_date]
 
 if (len(today_grouped) == 0):
